77d29e5f747f5d75dd2711de0688f31976babc3e
Ana Alice <anaalicd.cd@gmail.com>
2019-04-22 16:40:45 -0300

    terminando teste DOM

--- 1-DOM/js/script.js
+++ 1-DOM/js/script.js
@@ -1,2 +1,5 @@
 let titulo = document.querySelector("h1");
-titulo.textContent='Eu sou um titulo'
+titulo.textContent='Eu sou Maravilhosa';
+let texto = document.querySelector(".HERO");
+texto.style.color="#FFF";
+texto.style.backgroundColor="#F12375"
