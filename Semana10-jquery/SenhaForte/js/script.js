a449fe172e38cbe1abec8578539ddad59d2b947c
Ana Alice <anaalicd.cd@gmail.com>
2019-05-28 15:08:58 -0300

    aula de Jquery

--- Semana10-jquery/SenhaForte/js/script.js
+++ Semana10-jquery/SenhaForte/js/script.js
@@ -1,28 +1,36 @@
 $('#senha').keyup(function(){
     var senha = $(this).val();
-    var forca = 0;
+    var score = 0;
+    var forca="";
 
     if(senha.length  > 4){
-        forca+=25;
+        score+=25;
         console.log('TAMANHO');
 
     }
     var validaNumero = new RegExp(/[0-9]/);
     if(validaNumero.test(senha)){
-        forca+=25;
+        score+=25;
         console.log('NUMERO');
 
     }
     var validaMaiuscula = new RegExp(/[A-Z]/);
     if (validaMaiuscula.test(senha)){
-        forca+=25
+        score+=25
         console.log('MAIUSCULA');
 
     }
     var caracterEspecial = new RegExp(/[^a-zA-Z0-9]/);
     if(caracterEspecial.test(senha)){
-        forca+= 25;
+        score+= 25;
         console.log('ESPECIAIS');
+    }
+    if(score<50){
+        forca="Fraca";
+    }else if(score<75){
+        forca="Media"
+    }else{
+        forca="Forte"
     }
     $('#forca').html(`força ${forca} - Senha:${senha}` )
 
