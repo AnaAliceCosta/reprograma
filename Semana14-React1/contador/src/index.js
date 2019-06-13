5b327523304df15eecdaf4ca985c025a16984950
Ana Alice <anaalicd.cd@gmail.com>
2019-06-13 14:57:05 -0300

    trabalhando com status esconde-aparece

--- Semana14-React1/contador/src/index.js
+++ Semana14-React1/contador/src/index.js
@@ -1,10 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+class Contador extends React.Component{
+    constructor(props){
+        super(props)
+        this.state ={
+            count:0,
+            frase:"Contador não iniciado"
+        }
+
+    }
+    addUm=()=>{
+        this.setState((estadoAnterior)=>{
+            return {
+                count: estadoAnterior.count +1
+            }
+        })
+    }
+    subUm=()=>{
+        this.setState((estadoAnterior)=>{
+            return {
+                count: estadoAnterior.count === 0 ? 0 : (estadoAnterior.count -1)
+            }
+        })
+    }
+    reset=()=>{
+        this.setState(()=>{
+            return {
+                count: 0
+
+            }
+        })
+    }
+    render=()=>{
+        return(
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.addUm}>+1</button>
+                <button onClick={this.subUm}>-1</button>
+                <button onClick={this.reset}>reset</button>
+                <h3>{this.state.count===0?"Contador não iniciado": "Contador Iniciado"}</h3>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Contador />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
